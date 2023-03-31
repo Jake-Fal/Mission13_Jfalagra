312bcff01faca237ae20e0377cf8deec48ac05f6
slimmjimmy <jakefalagrady@gmail.com>
2023-03-30 22:42:21 -0600

    Fin

---
+++ src/Movies.tsx
@@ -0,0 +1,41 @@
+import data from './MovieData.json';
+
+const MDS = data.MovieData;
+function MovieList() {
+  return (
+    <>
+      <div>
+        <h3>Joel Hilton's Movie Collection</h3>
+      </div>
+      <div>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Category</th>
+              <th>Title</th>
+              <th>Year</th>
+              <th>Director</th>
+              <th>Rating</th>
+              <th>Edited</th>
+            </tr>
+          </thead>
+          <tbody>
+            {MDS.map((m) => (
+              <tr>
+                <td>{m.Category}</td>
+                <td>{m.Title}</td>
+                <td>{m.Year}</td>
+                <td>{m.Director}</td>
+                <td>{m.Rating}</td>
+                <td>{m.Edited}</td>
+              </tr>
+            ))}
+            <tr></tr>
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+}
+
+export default MovieList;
---
+++ src/Podcast.tsx
@@ -0,0 +1,12 @@
+function Podcast() {
+  return (
+    <>
+      <div className="text-center">
+        <h2>Podcast?!</h2>
+        <a href="https://baconsale.com/">Click for Da Bacon</a>
+      </div>
+    </>
+  );
+}
+
+export default Podcast;
